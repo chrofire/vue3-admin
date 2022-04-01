99be05797819a421dc2801182ba0a1ab17056024
chrofire <451939778@qq.com>
2022-04-01 21:02:48 +0800

    feat: 添加element-plus

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,35 @@ import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://cn.vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(), vueJsx()],
+    plugins: [
+        vue(),
+        vueJsx(),
+        AutoImport({
+            resolvers: [ElementPlusResolver({
+                importStyle: 'sass',
+                directives: true
+            })]
+        }),
+        Components({
+            resolvers: [ElementPlusResolver({
+                importStyle: 'sass',
+                directives: true
+            })]
+        })
+    ],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `@use "@/styles/element-preset.scss" as *;`
+            }
+        }
+    },
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
