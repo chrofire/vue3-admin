3e0766df3df9ee3eb33c0374ead0c827833c93ed
chrofire <451939778@qq.com>
2022-04-09 19:44:24 +0800

    feat: 添加面包屑

--- src/layout/TopBar/index.vue
+++ src/layout/TopBar/index.vue
@@ -1,16 +1,32 @@
 <template>
     <div class="top-bar">
-
+        <div class="breadcrumb">
+            <el-breadcrumb :separator-icon="ArrowRight">
+                <el-breadcrumb-item
+                    v-for="item in route?.meta?.breadcrumb || []"
+                    :key="item.name"
+                >
+                    <span>{{ item.name }}</span>
+                </el-breadcrumb-item>
+            </el-breadcrumb>
+        </div>
     </div>
 </template>
 
 <script setup>
+import { ArrowRight } from '@element-plus/icons-vue'
+import { useRoute } from 'vue-router'
 
+const route = useRoute()
 </script>
 
 <style lang="scss" scoped>
 .top-bar {
     height: 50px;
-    background-color: #fff566;
+    display: flex;
+    align-items: center;
+    .breadcrumb {
+        margin-left: 10px;
+    }
 }
 </style>
