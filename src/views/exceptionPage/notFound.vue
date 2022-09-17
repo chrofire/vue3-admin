57f2f1193bcf9c68926f244b80a0c6bb4b6742a7
chrofire <451939778@qq.com>
2022-09-17 16:20:00 +0800

    feat: 调整 notFound 页面

--- src/views/exceptionPage/notFound.vue
+++ src/views/exceptionPage/notFound.vue
@@ -1,6 +1,8 @@
 <template>
-    <div>
-        notFound
+    <div class="w-100% h-100% flex justify-center items-center">
+        <el-empty description="找不到页面">
+            <el-button plain @click="$router.back()">返回</el-button>
+        </el-empty>
     </div>
 </template>
 
